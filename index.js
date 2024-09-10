42bf524201648dc0efb789c814d4d69aef2f6973
iizeta <jiz.j12.a4@gmail.com>
2024-09-10 08:58:30 +0200

    Commit Inicial. Crear proyecto basado en NodeJS

---
+++ index.js
@@ -0,0 +1,13 @@
+// La única forma de sobrevivir, o al menos intentarlo es de la siguiente manera:
+
+// ✅ Crear un repositorio en Gitlab con un proyecto llamado 2324-hambruna-nombre-apellido
+// ✅ Crear un proyecto basado en NodeJS.
+// ✅ Crear un fichero index.js que ejecutaremos mediante node index.js
+// Crear un servicio para la ejecución asíncrona de petición de datos.
+// Cada línea de nuestro hechizo se mostrará con comentarios legibles, por ej:
+
+// 	“El donut con más azúcar es Black Mockaroo”
+
+
+
+  
